ffca00b1e6abf14d7cc1939d47955b1df1871d93
Kashif Khan <devkashifk@gmail.com>
2023-01-10 19:32:15 +0530

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About/About';
 import Blog from './components/Blog/Blog';
+import Blur from './components/Blur/Blur';
 import Contact from './components/Contact/Contact';
 import Protfolio from './components/Protfolio/Protfolio';
 import Resume from './components/Resume/Resume';
@@ -14,103 +15,7 @@ function App() {
     <>
     <Sidebar />
     <main className='main'>
-    <div className="blur" style={{
-        background : "var(--blur2)",
-        top : "05%",
-        width: "50rem",
-        height: "11rem"
-      }}></div>
-         <div className="blur" style={{
-        background : "var(--blur)",
-        top : "0%",
-        width: "40rem",
-        height: "11rem",
-        left: "-05rem",
-        zIndex : "10"
-      }}></div>
-       <div className="blur" style={{
-        background : "var(--blur3)",
-        top : "12%",
-        width: "50rem",
-        height: "11rem",
-        left: "-15rem",
-        zIndex : "10"
-      }}></div>
-        <div className="blur" style={{
-        background : "var(--blur2)",
-        top : "20%",
-        width: "50rem",
-        height: "11rem",
-        zIndex : "10"
-      }}></div>
-         <div className="blur" style={{
-        background : "var(--blur)",
-        top : "28%",
-        width: "40rem",
-        height: "11rem",
-        left: "-05rem",
-        zIndex : "10"
-      }}></div>
-       <div className="blur" style={{
-        background : "var(--blur3)",
-        top : "40%",
-        width: "50rem",
-        height: "11rem",
-        left: "-15rem",
-        zIndex : "10"
-      }}></div>
-        <div className="blur" style={{
-        background : "var(--blur2)",
-        top : "35%",
-        width: "50rem",
-        height: "11rem"
-      }}></div>
-         <div className="blur" style={{
-        background : "var(--blur)",
-        top : "50%",
-        width: "40rem",
-        height: "11rem",
-        left: "-05rem",
-      }}></div>
-       <div className="blur" style={{
-        background : "var(--blur2)",
-        top : "54%",
-        width: "50rem",
-        height: "11rem"
-      }}></div>
-       <div className="blur" style={{
-        background : "var(--blur3)",
-        top : "64%",
-        width: "50rem",
-        height: "11rem",
-        left: "-15rem",
-      }}></div>
-           <div className="blur" style={{
-        background : "var(--blur)",
-        top : "74%",
-        width: "40rem",
-        height: "11rem",
-        left: "-05rem",
-      }}></div>
-       <div className="blur" style={{
-        background : "var(--blur2)",
-        top : "76%",
-        width: "50rem",
-        height: "11rem"
-      }}></div>
-        <div className="blur" style={{
-        background : "var(--blur2)",
-        top : "90%",
-        width: "50rem",
-        height: "11rem",
-      }}></div>
-       <div className="blur" style={{
-        background : "var(--blur3)",
-        top : "95%",
-        width: "50rem",
-        height: "11rem",
-        left: "-15rem",
-      }}></div>
+      <Blur/>
       <Home />
       <About />
       <Services />
---
+++ src/components/Blur/Blur.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+export default function Blur() {
+  return (
+    <>
+    <div className="blur" style={{
+        background : "var(--blur2)",
+        top : "05%",
+        width: "50rem",
+        height: "11rem"
+      }}></div>
+         <div className="blur" style={{
+        background : "var(--blur)",
+        top : "0%",
+        width: "40rem",
+        height: "11rem",
+        left: "-05rem",
+        zIndex : "10"
+      }}></div>
+       <div className="blur" style={{
+        background : "var(--blur3)",
+        top : "12%",
+        width: "50rem",
+        height: "11rem",
+        left: "-15rem",
+      }}></div>
+        <div className="blur" style={{
+        background : "var(--blur2)",
+        top : "20%",
+        width: "50rem",
+        height: "11rem",
+        zIndex : "10"
+      }}></div>
+         <div className="blur" style={{
+        background : "var(--blur)",
+        top : "28%",
+        width: "40rem",
+        height: "11rem",
+        left: "-05rem",
+        zIndex : "10"
+      }}></div>
+       <div className="blur" style={{
+        background : "var(--blur3)",
+        top : "40%",
+        width: "50rem",
+        height: "11rem",
+        left: "-15rem",
+        zIndex : "10"
+      }}></div>
+        <div className="blur" style={{
+        background : "var(--blur2)",
+        top : "35%",
+        width: "50rem",
+        height: "11rem"
+      }}></div>
+         <div className="blur" style={{
+        background : "var(--blur)",
+        top : "50%",
+        width: "40rem",
+        height: "11rem",
+        left: "-05rem",
+      }}></div>
+       <div className="blur" style={{
+        background : "var(--blur2)",
+        top : "54%",
+        width: "50rem",
+        height: "11rem"
+      }}></div>
+       <div className="blur" style={{
+        background : "var(--blur3)",
+        top : "64%",
+        width: "50rem",
+        height: "11rem",
+        left: "-15rem",
+      }}></div>
+           <div className="blur" style={{
+        background : "var(--blur)",
+        top : "74%",
+        width: "40rem",
+        height: "11rem",
+        left: "-05rem",
+      }}></div>
+       <div className="blur" style={{
+        background : "var(--blur2)",
+        top : "76%",
+        width: "50rem",
+        height: "11rem"
+      }}></div>
+        <div className="blur" style={{
+        background : "var(--blur2)",
+        top : "90%",
+        width: "50rem",
+        height: "11rem",
+      }}></div>
+       <div className="blur" style={{
+        background : "var(--blur3)",
+        top : "95%",
+        width: "50rem",
+        height: "11rem",
+        left: "-15rem",
+      }}></div>
+      </>
+  )
+}
